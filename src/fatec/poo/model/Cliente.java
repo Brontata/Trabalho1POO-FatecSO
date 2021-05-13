474ceae6c26b9e0b052982fdbd6e8661fa984610
Guilherme Henrique <guilherme.henrique129@etec.sp.gov.br>
2021-05-13 19:18:22 -0300

    Criacao metodos da classe cliente

--- src/fatec/poo/model/Cliente.java
+++ src/fatec/poo/model/Cliente.java
@@ -10,6 +10,27 @@ package fatec.poo.model;
  * @author Guilherme Henrique Mello e Almeida
  * @author Luiz Miguel Jarduli Leite
  */
-public class Cliente {
+public class Cliente extends Pessoa{
+    
+    private double limiteCred;
+    private double limiteDisp;
+
+    public Cliente(double limiteCred, String cpf, String nome) {
+        super(cpf, nome);
+        this.limiteCred = limiteCred;
+        limiteDisp = limiteCred;
+    }
+    
+    public double getLimiteCred() {
+        return limiteCred;
+    }
+
+    public void setLimiteCred(double limiteCred) {
+        this.limiteCred = limiteCred;
+    }
+
+    public double getLimiteDisp() {
+        return limiteDisp;
+    }    
     
 }
